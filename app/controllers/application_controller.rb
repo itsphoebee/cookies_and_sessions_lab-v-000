7258df283654375eead28cfdae6cc873b894f143
Phoebe Lau <phoebelau7@gmail.com>
2018-02-05 20:03:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :cart
+  
   def cart
     cart = session[:cart] ||= []
     session[:cart] = cart
